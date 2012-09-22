94fa9814b4b5f350d4faddb3f20b9ae2d3fe4bad
Ricardo Zilleruelo <zeta@groupon.com>
2012-09-21 22:52:00 -0700

    ADD: update config to production services.

--- assets/javascript/config.js
+++ assets/javascript/config.js
@@ -1,13 +1,17 @@
+historyse_endpoint = "http://api.historyse.com/v1";
+auth_endpoint = "http://api.authorize-gateway.com/v1";
+
 var config = {
   client_id: "8b5376a0eb673623ad4b0b6b6d80dd92f553a2c3",
   historyse: {
-    endpoint: "http://historyse.local:43000/v1",
     window_events_sampling_rate: 5000,
-    endpoint_history_events: "http://historyse.local:43000/v1/users/%(user_id)s/history/events",
-    endpoint_history_urls: "http://historyse.local:43000/v1/users/%(user_id)s/history/urls",
+    endpoint: historyse_endpoint,
+    endpoint_history_events: historyse_endpoint + "/users/%(user_id)s/history/events",
+    endpoint_history_urls: historyse_endpoint + "/users/%(user_id)s/history/urls"
   },
   auth: {
-    endpoint_login: "http://authorize-gateway.com/v1/users/oauth",
+    endpoint: auth_endpoint,
+    endpoint_login: auth_endpoint + "/users/oauth",
     request_timeout: 1000
   }
 };
